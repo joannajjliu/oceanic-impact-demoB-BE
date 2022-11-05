3edbd81862d156865a8553e69b65f4c820e6bba4
Cameron Fairchild <cameron@opentensor.ai>
2022-11-05 14:52:47 -0400

    move app construction into db connect cb

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,10 +7,11 @@ dotenv.config({ // If $NODE_ENV is production, then .env.production will be load
     path: resolve(__dirname, `../.env${process.env.NODE_ENV === 'production' ? '.production' : '.development'}`)
 });
 
-const app_ = new App();
 const port = process.env.PORT || 3000; // If $PORT is not set, then 3000 will be used
 
 connectDatabase(process.env.MONGO_URI as string, process.env.MONGO_DB_NAME as string).then(() => {
+    const app_ = new App();
+
     app_.app.listen(port, () => {
         console.log(`Express server started on port ${port}`);
     });
